0ef2ffbc48f46b068db37d05dc72c66a32a8d483
Vicente Izquierdo <77403285+vif30@users.noreply.github.com>
2022-03-21 22:09:00 +0100

    Create Darts.py

---
+++ Darts.py
@@ -0,0 +1,13 @@
+"""System module."""
+from math import sqrt
+def score(x, y):
+  """Method Docs."""
+    h = round(sqrt((x ** 2) + (y ** 2)), 2)
+    if(h <= 1):
+        return 10
+    if(h > 1 and h <= 5):
+        return 5
+    if(h > 5 and h <= 10):
+        return 1
+    else:
+        return 0
