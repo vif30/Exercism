1545e9b899ba717a23ff988c4a3b19b36a3d40a1
Vicente Izquierdo <77403285+vif30@users.noreply.github.com>
2022-03-21 22:12:38 +0100

    Update Darts.py

--- Darts.py
+++ Darts.py
@@ -1,5 +1,8 @@
 """System module."""
 from math import sqrt
+"""Función que recibe unas coordenadas x e y y calcula la puntuación de dicho lanzamiento.
+Si la hipotenusa del triangulo que forman las coordenadas con el centro del circulo es <= 1 la puntuacion es 10,
+si es entre 1 y 5 será de 5 puntos, si es de entre 5 y 10, se asignara 1 punto y si es mayor a estos, sera 0 puntos"""
 def score(x, y):
   """Method Docs."""
     h = round(sqrt((x ** 2) + (y ** 2)), 2)
