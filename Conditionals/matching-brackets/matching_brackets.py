6992ae4425f4b82e4ad6a74822343c0e68c90f62
vif30 <izquierdovicente@gmail.com>
2022-07-08 22:34:48 +0200

    Update matching_brackets.py

--- Conditionals/matching-brackets/matching_brackets.py
+++ Conditionals/matching-brackets/matching_brackets.py
@@ -1,2 +1,27 @@
 def is_paired(input_string):
-    pass
+    openings = ("(","[","{")
+    closings = (")","]","}")
+    open = []
+    for i in input_string:
+        if i in openings:
+            open.append(i)
+        if i in closings:
+            if len(open) == 0:
+                return False
+            paired = matched(i, open)
+            if paired:
+                open.pop()
+            else:
+                return False
+    if len(open) != 0:
+        return False
+    return True
+
+def matched(last, open):
+    if last == "]":
+        return bool("[" == open[len(open)-1])
+    if last == ")":
+        return bool("(" == open[len(open)-1])
+    if last == "}":
+        return bool("{" == open[len(open)-1])
+
