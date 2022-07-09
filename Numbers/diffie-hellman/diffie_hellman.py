0130ab3d8eeae0fe30ad3accb05fdaf83fd2abe4
vif30 <izquierdovicente@gmail.com>
2022-07-09 11:57:44 +0200

    diffie

--- Numbers/diffie-hellman/diffie_hellman.py
+++ Numbers/diffie-hellman/diffie_hellman.py
@@ -1,8 +1,9 @@
+import random
 def private_key(p):
     if(p < 1):
          raise ValueError("Only positive integers are allowed")
     else:
-        return p
+        return random.randint(1, p)
 
 def public_key(p, g, private):
     A = pow(g, private) % p
--- Numbers/palindrome-products/palindrome_products.py
+++ Numbers/palindrome-products/palindrome_products.py
@@ -1,24 +1,37 @@
-def largest(min_factor, max_factor):
-    if(min_factor > max_factor):
-        raise ValueError("min must be <= max")
-    
-    for i in range(max_factor, min_factor, -1):
-        for j in range(max_factor, min_factor+1, -1):
-            prod = i*j
-            if prod in range(1, 10):
-                result = (prod, (i,j))
-                return result
-            if str(i*j) == str(i*j)[::-1]:
-                result = (i*j, (i,j))
-                return result
+from math import floor, ceil
+def smallest(max_factor, min_factor):
+    validate(min_factor, max_factor)
+    factors  = range(min_factor, max_factor + 1)
+    products = range(pow(min_factor, 2), pow(max_factor, 2) + 1)
+    return palindrome_products(products, factors)
 
-print(largest(1, 9))
-def smallest(min_factor, max_factor):
-    if(min_factor > max_factor):
-        raise ValueError("min must be <= max")
-    
-    for i in range(min_factor, max_factor):
-        for j in range(min_factor, max_factor+1):
-            if str(i*j) == str(i*j)[::-1]:
-                result = (i*j, (i,j))
-                return result
+def largest(max_factor, min_factor):
+    validate(min_factor, max_factor)
+    factors  = range(min_factor, max_factor + 1)
+    # this range is descending, to find the largest faster
+    products = range(pow(max_factor, 2), pow(min_factor, 2) - 1, -1)
+    return palindrome_products(products, factors)
+
+def validate(minf, maxf):
+    if minf > maxf:
+        raise ValueError('min must be <= max')
+
+def palindrome_products(products, factors):
+    for product in products:
+        if is_palindrome(product):
+            pairs = []
+            for i in factors:
+                j, rem = divmod(product, i)
+                if rem == 0 and j in factors:
+                    pairs.append([i, j])
+            if len(pairs) != 0:
+                # the first one found is the smallest/largest,
+                # so we can return immediately
+                return (product, pairs)
+    return (None, [])
+
+def is_palindrome(number):
+    s = str(number)
+    return s[:floor(len(s)/2)] == s[-1:ceil(len(s)/2)-1:-1]
+
+print(largest(99,10))
