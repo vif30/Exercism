e29e895c75891ef78d492de22314cf1a71d75881
vif30 <izquierdovicente@gmail.com>
2022-07-10 00:27:48 +0200

    Update tuples.py

--- Tuples/tisbury-treasure-hunt/tuples.py
+++ Tuples/tisbury-treasure-hunt/tuples.py
@@ -13,14 +13,13 @@ def compare_records(azara_record, rui_record):
     return bool(azara_converted == rui_record[1])
 
 def create_record(azara_record, rui_record):
-    if(compare_records(azara_record, rui_record)):
+    if compare_records(azara_record, rui_record):
         return azara_record + rui_record
-    else:
-        return "not a match"
+    return "not a match"
 
 def clean_up(combined_record_group):
     texto = ""
-    for i in combined_record_group:
-        cleaned = ((i[0], i[2], i[3], i[4]))
+    for index in combined_record_group:
+        cleaned = ((index[0], index[2], index[3], index[4]))
         texto += str(cleaned) + "\n"
     return texto
